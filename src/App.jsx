aee20cd3c217d17b3a88211c28adfde33a98f76a
Typhon25 <abhinavchouhan117@gmail.com>
2024-09-20 11:49:44 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Card from './components/card.jsx'
+import './App.css'
+
+export const App = () => {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <Card />
+    </div>
+  )
+}
+
